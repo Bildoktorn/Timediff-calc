d0b7d63538cd38e4a78cd98b40813bc527e6c98b
Bildoktorn <MPALundberg@gmail.com>
2015-11-07 00:17:33 +0100

    Improved workflow, more explicit.
    Changed to a clear workflow from run start to run end without requiring
    external tools like calc or repeated subtraction of load times

--- Timecalc.py
+++ Timecalc.py
@@ -1,12 +1,26 @@
 import datetime
-print ('Hi hello')
+print ('Hi. Enter timestamps as \'hours minutes seconds milliseconds\' without quotes.\n')
 while 1:
-    time1 = [int(x) for x in input ('First time? ').split()]
-    time2 = [int(x) for x in input ('Second time? ').split()]
+    loads = []
     try:
-        time1 = datetime.timedelta(0, time1[2], 0, time1[3], time1[1], time1[0])
-        time2 = datetime.timedelta(0, time2[2], 0, time2[3], time2[1], time2[0])
-        print (str(time2-time1) + '\n')
-    except:
-        print (time1, time2)
-        print ('Error, incorrect input. Input as hours minutes seconds milliseconds only separated by space.')
+        runstart = [int(x) for x in input ('Run starts at? ').split()]
+        while 1:
+            loadstart = [int(x) for x in input ('Load starts at? Type \'0\' without quotes if none or finished. ').split()]
+            if loadstart == [0]:
+                break
+            loadend = [int(x) for x in input ('Load ends at? ').split()]
+            loadstart = datetime.timedelta(0, loadstart[2], 0, loadstart[3], loadstart[1], loadstart[0])
+            loadend = datetime.timedelta(0, loadend[2], 0, loadend[3], loadend[1], loadend[0])
+            loads.append(loadend-loadstart)            
+        runend = [int(x) for x in input ('Run ends at? ').split()]
+        runstart = datetime.timedelta(0, runstart[2], 0, runstart[3], runstart[1], runstart[0])
+        runend = datetime.timedelta(0, runend[2], 0, runend[3], runend[1], runend[0])
+        runtime = runend-runstart
+        if loads:
+            for x in loads:
+                runtime -= x
+            
+        print (str(runtime) + '\n')
+    except (ValueError, IndexError):
+        print ('Incorrect input. Input as \'hours minutes seconds milliseconds\' without quotes.')
+    
